77eaa83d076b105d459a356f1d265070991a3e3f
krzysztofjesien <krzysztof@koii.network>
2023-10-22 22:53:47 +0200

    Initial version UI page with Chart

---
+++ app/components/navigation.tsx
@@ -0,0 +1,10 @@
+export default function Navigation() {
+  return (
+    <nav>
+      <div>
+        <h1>Ethereum Average Gas Price</h1>
+        <small>Next.js showcase app</small>
+      </div>
+    </nav>
+  );
+}
